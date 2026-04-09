15cadad61c2c31b8888ddad61fccad19af18f25d
CamiloParraS <juanparracsfn@gmail.com>
2026-04-09 18:58:02 -0500

    Add Fisher-Yates shuffle utilities
    Provide fisherYates (in-place) and shuffled (returns a new array)

---
+++ src/ds/shuffle.ts
@@ -0,0 +1,12 @@
+export function fisherYates<T>(items: T[]): T[] {
+  for (let index = items.length - 1; index > 0; index -= 1) {
+    const swapIndex = Math.floor(Math.random() * (index + 1));
+    [items[index], items[swapIndex]] = [items[swapIndex], items[index]];
+  }
+
+  return items;
+}
+
+export function shuffled<T>(items: T[]): T[] {
+  return fisherYates([...items]);
+}
