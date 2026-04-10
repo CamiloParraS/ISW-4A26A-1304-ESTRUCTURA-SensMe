c1c3ede746942b7944aebed002e6262c0ca53bb7
CamiloParraS <juanparracsfn@gmail.com>
2026-04-10 00:49:15 -0500

    Add audio ingestion constants
    Provide AUDIO_EXTENSIONS set, MusicBrainz and CoverArtArchive base URLs.
    Also export MB_RATE_LIMIT_MS for API throttling

---
+++ src/ingestion/constants.ts
@@ -0,0 +1,14 @@
+export const AUDIO_EXTENSIONS = new Set([
+  ".mp3",
+  ".flac",
+  ".aac",
+  ".ogg",
+  ".wav",
+  ".m4a",
+  ".opus",
+  ".wma",
+]);
+
+export const MUSICBRAINZ_BASE = "https://musicbrainz.org/ws/2";
+export const CAA_BASE = "https://coverartarchive.org";
+export const MB_RATE_LIMIT_MS = 1100;
