46652b2b322dc5047515f8822bfcaab1af42db58
CamiloParraS <juanparracsfn@gmail.com>
2026-04-13 19:52:03 -0500

    feat: add Sonner toast notifications with themed support; update ToastProvider to use new Toaster component

--- src/components/ToastProvider.tsx
+++ src/components/ToastProvider.tsx
@@ -1,18 +1,15 @@
 import type { ReactNode } from "react";
-import { useToastProvider } from "../hooks/useToast";
-import { ToastViewport } from "./ToastViewport";
+import { Toaster } from "sonner";
 
 interface ToastProviderProps {
     children: ReactNode;
 }
 
 export function ToastProvider({ children }: ToastProviderProps) {
-    const { toasts, dismiss } = useToastProvider();
-
     return (
         <>
             {children}
-            <ToastViewport toasts={toasts} onDismiss={dismiss} />
+            <Toaster position="top-right" richColors />
         </>
     );
 }
